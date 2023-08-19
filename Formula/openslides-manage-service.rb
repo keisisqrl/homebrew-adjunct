d326bc3f34b6fe03b95423b18a3e5b6f424082c0
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-08-19 13:41:27 -0700

    openslides-manage-service: use std_go_args

--- Formula/openslides-manage-service.rb
+++ Formula/openslides-manage-service.rb
@@ -3,13 +3,13 @@ class OpenslidesManageService < Formula
   homepage "https://github.com/openslides/openslides"
   url "https://github.com/OpenSlides/openslides-manage-service/archive/refs/tags/4.0.0.tar.gz"
   sha256 "d67ca416ef01ee7845dc7df4aae1b5c1ee808174019f92021ecf4cc10bde93f1"
+  revision 1
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "make", "go-build"
-    bin.install "openslides"
+    system "go", "build", *std_go_args(output: bin/"openslides"), "./cmd/openslides"
   end
 
   test do
