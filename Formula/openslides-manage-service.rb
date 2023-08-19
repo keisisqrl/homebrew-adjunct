ff91e232cdb969eae0730aa0323dd5bf4d36fe9a
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-08-19 13:44:52 -0700

    openslides-manage-service: fix style violation

--- Formula/openslides-manage-service.rb
+++ Formula/openslides-manage-service.rb
@@ -3,8 +3,8 @@ class OpenslidesManageService < Formula
   homepage "https://github.com/openslides/openslides"
   url "https://github.com/OpenSlides/openslides-manage-service/archive/refs/tags/4.0.0.tar.gz"
   sha256 "d67ca416ef01ee7845dc7df4aae1b5c1ee808174019f92021ecf4cc10bde93f1"
-  revision 1
   license "MIT"
+  revision 1
 
   depends_on "go" => :build
 
