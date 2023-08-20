5940ae2a953c93beaece1449d634359b1315702f
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-08-20 19:12:29 +0000

    noteshrink: add 0.1.1 bottle.

--- Formula/noteshrink.rb
+++ Formula/noteshrink.rb
@@ -7,6 +7,12 @@ class Noteshrink < Formula
   sha256 "f910667247aea13c7f0ba86d26a204d53d8c4246ae5fb50214c72c52af362588"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/keisisqrl/homebrew-adjunct/releases/download/noteshrink-0.1.1"
+    sha256 cellar: :any_skip_relocation, ventura:      "0924e2cd6fa6d39dcb573fc5cc5c61ca7380d022818f651d8e9121f39208860b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d41c0c124466b370bb6c4ca127f55aee3a9a10c7052c15d8d404320af7511df2"
+  end
+
   depends_on "imagemagick"
   depends_on "numpy"
   depends_on "pillow"
