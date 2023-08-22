f9fc1ac49ff7cebe4283af7f2cb8c153a27445a3
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-08-22 07:15:52 +0000

    droidstar: add 6d73e2e bottle.

--- Formula/droidstar.rb
+++ Formula/droidstar.rb
@@ -6,6 +6,12 @@ class Droidstar < Formula
   version "6d73e2e"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/keisisqrl/homebrew-adjunct/releases/download/droidstar-6d73e2e"
+    sha256 cellar: :any,                 ventura:      "29b8eaccf64f7a2d0af3439aec7f32b54b576a21a65e94d3c1e933bdb5c74309"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "53a088e756816bef25b782a35fb58f6d27f49f446cd7acd82bb8cc938e394ffe"
+  end
+
   depends_on "imbe_vocoder"
   depends_on "qt@5"
 
