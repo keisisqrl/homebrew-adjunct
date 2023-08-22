75a222280f870afa64eaad2ecfc4e1d379bf8fce
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-08-22 07:15:52 +0000

    imbe_vocoder: add 03423cb bottle.

--- Formula/imbe_vocoder.rb
+++ Formula/imbe_vocoder.rb
@@ -7,6 +7,12 @@ class ImbeVocoder < Formula
   # sha256 ""
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/keisisqrl/homebrew-adjunct/releases/download/imbe_vocoder-03423cb"
+    sha256 cellar: :any_skip_relocation, ventura:      "66f8db5104695251d4cb44923da497c1521b771ab359dba71288fe89d4a5f5b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8a2fd4720b3b230490413155e179ed18bec642c9557b62120ef06769fc5c00a0"
+  end
+
   def install
     system "make"
     lib.install "libimbe_vocoder.a"
