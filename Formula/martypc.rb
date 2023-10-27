f1e7ea446dac3d0065b417001b753b0127ebfb32
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-10-27 01:34:08 +0000

    martypc: add 0.1.3 bottle.

--- Formula/martypc.rb
+++ Formula/martypc.rb
@@ -10,6 +10,12 @@ class Martypc < Formula
     regex(/^(\d+?\.\d+?\.\d+?)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/keisisqrl/homebrew-adjunct/releases/download/martypc-0.1.3"
+    sha256 cellar: :any_skip_relocation, ventura:      "a0b03db598a39a73cefaa2ca912da735d111782f06159e052924dab90858907b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "17f6ca8abd4e665060841aa4e9decd81b037a3a9da129d1553f1ef38f6ac9ca6"
+  end
+
   depends_on "rust" => :build
   on_linux do
     depends_on "alsa-lib"
