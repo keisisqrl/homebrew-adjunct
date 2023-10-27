8326fa7170deb311d407f60922e45f483bb8ad14
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-10-27 04:29:47 +0000

    kevedit: add 1.2.0 bottle.

--- Formula/kevedit.rb
+++ Formula/kevedit.rb
@@ -6,6 +6,12 @@ class Kevedit < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/cknave/kevedit.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/keisisqrl/homebrew-adjunct/releases/download/kevedit-1.2.0"
+    sha256 ventura:      "0b1b7910187bc10db16c23f33eb00ad746dd5d9d222d29dbaff7ab124c844e43"
+    sha256 x86_64_linux: "a88187a4083d54b98488cb230893fa225b7ba52df5de042b276c2f7712c85677"
+  end
+
   depends_on "coreutils" => :build
   depends_on "pkg-config" => :build
   depends_on "sdl2"
