4b906895403a2ac0a6d8b65d0a21698affebc79b
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-11-12 14:38:00 -0800

    add cask: Intiface Central

---
+++ Casks/intiface-central.rb
@@ -0,0 +1,11 @@
+cask "intiface-central" do
+  version "2.5.1"
+  sha256 "894af0642a2415a019424a7e0fbed1ac4bf96a6bcc58436fc7cb85c65a7b36e1"
+
+  url "https://github.com/intiface/intiface-central/releases/download/v#{version}/intiface-central-v#{version}-macos-universal.zip"
+  name "Intiface Central"
+  desc "Intiface Central (Buttplug Frontend) Application for Desktop and Mobile"
+  homepage "https://intiface.com/central/"
+
+  app "Intiface Central.app"
+end
