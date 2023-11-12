d845391bbc83f34ca4daa38b96cf732779cf31c7
Amy W <49384+keisisqrl@users.noreply.github.com>
2023-11-12 14:47:50 -0800

    fix some stuff in the thing
    Closes #10.
    
    Signed-off-by: Amy W <49384+keisisqrl@users.noreply.github.com>

--- Casks/intiface-central.rb
+++ Casks/intiface-central.rb
@@ -2,10 +2,17 @@ cask "intiface-central" do
   version "2.5.1"
   sha256 "894af0642a2415a019424a7e0fbed1ac4bf96a6bcc58436fc7cb85c65a7b36e1"
 
-  url "https://github.com/intiface/intiface-central/releases/download/v#{version}/intiface-central-v#{version}-macos-universal.zip"
+  url "https://github.com/intiface/intiface-central/releases/download/v#{version}/intiface-central-v#{version}-macos-universal.zip",
+      verified: "github.com/intiface/intiface-central/"
   name "Intiface Central"
-  desc "Intiface Central (Buttplug Frontend) Application for Desktop and Mobile"
+  desc "Buttplug Frontend Application for Desktop and Mobile"
   homepage "https://intiface.com/central/"
+
+  livecheck do
+    strategy :github_releases
+  end
+
+  depends_on macos: ">= :mojave"
 
   app "Intiface Central.app"
 end
